1075612c7a3a013418a5a0ae6a1304afb25680e8
Onur <onur1992@gmail.com>
2019-06-30 23:50:12 +0300

    Creation of InputManager and Character Controller

---
+++ DOTCv02/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputManager), typeof(Rigidbody))]
+public class CharacterController : MonoBehaviour
+{
+    public float characterSpeed;
+
+    private Rigidbody characterRigidbody;
+    private CapsuleCollider characterCollider;
+    private Animator characterAnimator;
+    private InputManager inputManager;
+
+    private void Awake()
+    {
+        characterRigidbody = GetComponent<Rigidbody>();
+        characterCollider = GetComponent<CapsuleCollider>();
+        characterAnimator = GetComponentInChildren<Animator>();
+        inputManager = GetComponent<InputManager>();
+    }
+
+    private void Update()
+    {
+        ProcessInputs();
+    }
+
+    private void ProcessInputs()
+    {
+        characterAnimator.SetFloat("Horizontal", inputManager.HorizontalInput);
+        characterRigidbody.AddForce(new Vector3(inputManager.HorizontalInput,0,0).normalized*characterSpeed);
+        characterAnimator.SetFloat("Vertical", inputManager.VericalInput);
+        characterRigidbody.AddForce(new Vector3(0, 0, inputManager.VericalInput).normalized * characterSpeed);
+
+        if (inputManager.HorizontalInput == 0 && inputManager.VericalInput == 0)
+        {
+            characterRigidbody.velocity = Vector3.zero;
+            characterRigidbody.angularVelocity = Vector3.zero;
+        }
+
+        if (inputManager.Shoot == true)
+            characterAnimator.SetTrigger("Shoot");
+
+        if (inputManager.Cast == true)
+            characterAnimator.SetTrigger("Cast");
+    }
+}
---
+++ DOTCv02/Assets/Scripts/InputManagers/KeyboardInputManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInputManager : InputManager
+{
+    private void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+
+        _shoot = Input.GetKey(KeyCode.Mouse0);
+        _cast = Input.GetKey(KeyCode.Mouse1);
+    }
+}
