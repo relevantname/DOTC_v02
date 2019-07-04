a9ff20757f6f91fc470e705b54b6486041b4e2f0
Onur <onur1992@gmail.com>
2019-07-04 23:48:34 +0300

    Adding some shooting logic

---
+++ DOTCv02/Assets/Scripts/Animation/ParentAnimationEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class responsible for transmit animation events to parent gameobject scrtips.
+/// </summary>
+public class ParentAnimationEventHandler : MonoBehaviour, IAnimationEventHandler<string>
+{
+    public void HandleEvent(string eventParameter)
+    {
+        transform.parent.SendMessage(eventParameter);
+    }
+}
---
+++ DOTCv02/Assets/Scripts/Objects/MagicBall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBall : MonoBehaviour, IShootableObject
+{
+    private Rigidbody magicBallRigidbody;
+
+
+    private float damage;
+    private float moveSpeed;
+
+    public void Initialize(float damage, float moveSpeed)
+    {
+    }
+}
